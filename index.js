4213e2dac93be15d36dc1ef7e7ddccef47e20dde
Bryndon Humphrey <bryndon.humphrey@bryndon.humphrey>
2021-08-25 14:42:41 -0700

    working but need refinement

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
 // Add your javascript here
-const pokedex = document.getElementById('pokedex');
+(function(dom, launcher , _){
+    var dom = pendo.dom
+    var pokedex2 = document.getElementById('pendo-resource-center--pokedex');
+    var launcherDiv = dom('.pendo-resource-center--container');
+    /*
+    var clickableCard = document.querySelector('.pendo-resource-center--card');
+    */
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 150; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
+    var fetchPokemon2 = () => {
+        const promises = [];
+        for (let i = 1; i <= 150; i++) {
+            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+            promises.push(fetch(url).then((res) => res.json()));
+        }
+        Promise.all(promises).then((allPokemon) => {
+            const pokemon = allPokemon.map((result) => ({
+                name: result.name,
+                image: result.sprites['front_default'],
+                type: result.types.map((type) => type.type.name).join(', '),
+                id: result.id,
+                move: result.moves.map((move) => move.move.name).join(', '),
+            }));
+            displayPokemon2(pokemon);
+        });
+    };
+
+    var displayPokemon2 = (pokemon) => {
+        const pokemonHTMLString = pokemon
+            .map((poke) => 
+            `
+            <li data-pokeId="${poke.id}" class="pendo-resource-center--card">
+                <h2 data-pokeId="${poke.id}" class="pendo-resource-center--card-title">#${poke.id}. ${poke.name} <span data-pokeId="${poke.id}"> <img data-pokeId="${poke.id}" class="pendo-resource-center--card-image" src="${poke.image}"/></span></h2>
+                <div data-pokeId="${poke.id}" id="poke-details-${poke.id}" class="pendo-resource-center--details pendo-resource-center--collapsed">
+                    <p data-pokeId="${poke.id}" class="pendo-resource-center--card-subtitle">Type: ${poke.type}</p>
+                    <p data-pokeId="${poke.id}" class="pendo-resource-center--card-subtitle">Moves: ${poke.move}</p>
+                <div>
+            </li>
+            `
+            )
+            .join('');
+        pokedex2.innerHTML = pokemonHTMLString;
+    };
+
+    fetchPokemon2();
+
+    launcherDiv.on('click', '.pendo-resource-center--card', function(e) {
+        var pokeId = dom(eventTarget(e))[0].dataset.pokeId;
+        togglePendoResourceCenterClass(pokeId)
+    })
+
+    function eventTarget(e) {
+        return (e && e.target) || e.srcElement;
     }
-    Promise.all(promises).then((allPokemon) => {
-        const pokemon = allPokemon.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id,
-            move: result.moves.map((move) => move.move.name).join(', '),
-        }));
-        displayPokemon(pokemon);
-    });
-};
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon
-        .map((poke) => 
-        `
-        <li class="card">
-            <img class="card-image" src="${poke.image}"/>
-            <h2 class="card-title">#${poke.id} - ${poke.name}</h2>
-            <div class="details>
-                <p class="card-subtitle">Type: ${poke.type}</p>
-                <p class="card-subtitle">Moves: ${poke.move}</p>
-            <div>
-        </li>
-        `
-        )
-        .join('');
-    pokedex.innerHTML = pokemonHTMLString;
-};
+    function togglePendoResourceCenterClass(id) {
+        var pokeDetailId = document.getElementById(`poke-details-${id}`);
+        pokeDetailId.classList.toggle("pendo-resource-center--collapsed");
+    }
 
-fetchPokemon();
+})(pendo.dom, pendo.BuildingBlocks.BuildingBlockResourceCenter, pendo._);
